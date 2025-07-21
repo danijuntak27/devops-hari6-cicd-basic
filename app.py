204509afe894796798e18c7f1b701051db1dc574
danijuntak27 <dani.simanjuntak@asetku.com>
2025-07-21 07:35:17 +0000

    Ubah return jadi versi V2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "✅ Hello dari CI/CD GitHub Action DANIJUNTAK !"
+    return "✅ Hello dari CI/CD GitHub Action DANIJUNTA v2 !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
